a9d5680b9cb2dc5535590d68ab06f5439301a155
duk7292 <136354450+duk7292@users.noreply.github.com>
2023-09-04 16:16:24 +0200

    finished Project
    the network gets up to 85% which is enough for me for now.

--- MainFiles/main.py
+++ MainFiles/main.py
@@ -18,7 +18,7 @@ image_size = 28
 images_2d = images_1d.reshape(-1, image_size, image_size)
 
 labels_train, labels_test, images_train, images_test = train_test_split(labels, images_2d, test_size=0.1)
-lrList = [5e-3,3e-3,1e-3,5e-4,3e-4,1e-4,5e-5,3e-5,1e-5,5e-6]
+lrList = [1e-3,1e-3,1e-3,1e-3,5e-4 ,5e-4,5e-4,1e-5,1e-5,5e-6]
 lr = lrList[0]
 
 
@@ -27,17 +27,17 @@ lr = lrList[0]
 
 
 layers = [
-    ut.ConvolutionLayer(6,3,lr),
+    ut.ConvolutionLayer(32,3,lr),
     ut.MaxPooling2D(2),
     ut.ReLuLayer(),
     ut.ConvolutionLayer(16,3,lr),
     ut.MaxPooling2D(2),
     ut.FlattenLayer(),
-    ut.DenseLayer(180,4704,lr),
+    ut.DenseLayer(1000,25088,lr),
     ut.ReLuLayer(),
-    ut.DenseLayer(80,180,lr),
+    ut.DenseLayer(70,1000,lr),
     ut.ReLuLayer(),
-    ut.DenseLayer(26,80,lr),
+    ut.DenseLayer(26,70,lr),
     ut.SoftmaxLayer()
 ]
 
@@ -52,40 +52,64 @@ def test(test_size = 1000):
         if (output == label):
             correct+=1
     print((correct/test_size)*100,"%")
-
-def train(batch_size = 250):
+def train(batch_size = 250 ,back_cluster_size = 1):
+    st = time.time()
     global lr
     global lrList
-    num_batches = len(images_train) // batch_size
+    outputSize = 26
+    num_batches = (len(images_train) // batch_size) //back_cluster_size
+
+    cluster_num = batch_size// back_cluster_size
     accuracies = []
-    for epoch in range(1000):
+    labels_guessed = np.zeros((26,2))
+    for epoch in range(1000000):
         for batch_idx in range(num_batches):
-            start_idx = batch_idx * batch_size
-            end_idx = start_idx + batch_size
+            if(batch_idx % 10 == 0):
+                
+                labels_guessed = np.zeros((26,2))
             batch_predictions = []
             batch_labels = []
-            layers[0].learning_rate = lr
-            layers[3].learning_rate = lr
-            layers[6].learning_rate = lr
-            layers[8].learning_rate = lr
-           # layers[10].learning_rate = lr
+            
+            layers[0].setLR(lr)
+            layers[3].setLR(lr)
+            layers[6].setLR(lr)
+            layers[8].setLR(lr)
+            layers[10].setLR(lr)
+            
+
+
             t = time.time()
-            for i, (image, label) in enumerate(zip(images_train[start_idx:end_idx], labels_train[start_idx:end_idx])):
-                output = ut.CNN_forward(image, layers)
-                label_arr = np.zeros(len(output))
-                label_arr[label] = 1
-                batch_predictions.append(output)
-                batch_labels.append(label)
-                error =(output- label_arr)
-                ut.CNN_backward(error, layers)
+            for cluster in range(cluster_num):
+                start_idx = batch_idx * batch_size +(cluster*back_cluster_size)
+                end_idx = start_idx + back_cluster_size
+                cluster_error = np.zeros((back_cluster_size,outputSize))
+                for i, (image, label) in enumerate(zip(images_train[start_idx:end_idx], labels_train[start_idx:end_idx])):
+                    output = ut.CNN_forward(image, layers)
+                    label_arr = np.zeros(len(output))
+                    label_arr[label] = 1
+                    batch_predictions.append(output)
+                    batch_labels.append(label)
+                    labels_guessed[label][0] += 1
+                    
+                    if (label == np.argmax(output)):
+                        labels_guessed[label][1] += 1
+
+                    error =(output- label_arr)
+                    cluster_error[i] = error
+                avg_Error =  np.mean(cluster_error, axis=0)
+                ut.CNN_backward(avg_Error, layers)
 
             print(time.time()-t)
             accuracy = ut.calculate_accuracy(batch_predictions, batch_labels)
             accuracies.append(accuracy) 
-
+            for i,l in enumerate(labels_guessed):
+                print(chr(i + 97),round(((l[1]/l[0])*100),2),"% Total:" , l[0])
             print(f"Genauigkeit für Batch {(epoch*num_batches)+batch_idx + 1}: {accuracy:.2f}%\nDurchschnittliche Genauigkeit über alle Batches: { np.mean(accuracies):.2f}%\nHöste Genauigkeit bis Jetzt {accuracies[np.argmax(accuracies)]:.2f}% bei batch {np.argmax(accuracies)+1}")
-            if(accuracy >98.5 ):
-                return
+            
+
+            
+            if(accuracy >85 ):
+                return (time.time()-st)
             
             learning_rate_idx = int(accuracy // 10)
 
@@ -93,5 +117,5 @@ def train(batch_size = 250):
 
 
         
-train()
+print(train())
 test()
--- MainFiles/utils.py
+++ MainFiles/utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 from scipy.signal import convolve2d
-from scipy.ndimage import maximum_filter
 import cupy as cp
-import time
 class ConvolutionLayer:
     def __init__(self,kernel_num,kernel_size,learning_rate) -> None:
         self.learning_rate = learning_rate
@@ -21,7 +19,7 @@ class ConvolutionLayer:
         
         for kernel,bias in zip(self.kernels,self.biases):
             for image in images:
-                kernel_conv =  convolve2d(image, kernel, mode='same')#conv2d_same(image,kernel)
+                kernel_conv =  convolve2d(image, kernel, mode='same')
 
                 
                 kernel_conv += bias
@@ -39,18 +37,19 @@ class ConvolutionLayer:
         for i, (kernel, bias) in enumerate(zip(self.kernels, self.biases)):
             
             for j, image in enumerate(self.input):
-                dKernels[i] +=convolve2d(image,  errors[i], mode='valid') #conv2d_valid(image, errors[i])
+                dKernels[i] +=convolve2d(image,  errors[i], mode='valid') 
                 dBiases[i] += np.sum(errors[i])
 
                 
                 
 
                 rotated_kernel = np.rot90(kernel, 2)
-                dInputs[j] += convolve2d(errors[i], rotated_kernel, mode='same')#conv2d_same(errors[i], rotated_kernel)
+                dInputs[j] += convolve2d(errors[i], rotated_kernel, mode='same')
         self.kernels -= self.learning_rate * dKernels
         self.biases -= self.learning_rate * dBiases
         return dInputs
-
+    def setLR(self,lr):
+        self.learning_rate = lr
 class MaxPooling2D:
     def __init__(self,kernel_size) -> None:
         self.kernel_size = kernel_size
@@ -66,9 +65,7 @@ class MaxPooling2D:
 
         enlarged_pooled_output = pooled_output.repeat(1, axis=0).repeat(self.kernel_size, axis=1).repeat(self.kernel_size, axis=2)
         self.max_mask = (images == enlarged_pooled_output).astype(int)
-        correct_max_mask_sum = int((images.shape[0]*(images[0].shape[0]*images[0].shape[1]))/self.kernel_size**2)
-        if(np.sum(self.max_mask) > correct_max_mask_sum):
-            self.max_mask =  correct_max_mask(self.max_mask,self.kernel_size)
+        
         
         return pooled_output
 
@@ -96,20 +93,17 @@ class DenseLayer:
     
         self.input = None
     def forward(self, input):
-        self.input = np.array(input)  # Eingang als NumPy-Array
+        self.input = np.array(input)  
         if(self.cupy_enabled):
             
             input_cp = cp.asarray(self.input)
             
 
-            # Berechnung mit CuPy
             output_cp = cp.dot(self.weights, input_cp) + (self.bias.reshape(self.output_size))
 
-            # Konvertiere das CuPy-Array zurück zu einem NumPy-Array
             output = cp.asnumpy(output_cp)
 
         else:  
-        # Berechnung mit NumPy
             output = np.dot(self.weights, self.input) + (self.bias.reshape(self.output_size))
 
         return output
@@ -122,12 +116,10 @@ class DenseLayer:
             error_cp = cp.asarray(error)
             input_reshaped_cp = cp.asarray(input_reshaped)
             
-            # Berechnung mit CuPy
             dW = cp.dot(error_cp, input_reshaped_cp)
             db = cp.sum(error_cp, axis=1)
             dA_prev_cp = cp.dot(cp.transpose(self.weights), error_cp)
 
-            # Konvertiere die CuPy-Arrays zurück zu NumPy-Arrays
             
             dA_prev = cp.asnumpy(dA_prev_cp)
 
@@ -140,7 +132,8 @@ class DenseLayer:
         self.bias -= self.learning_rate * db
         self.weights -= self.learning_rate *  dW
         return dA_prev.ravel()
-
+    def setLR(self,lr):
+        self.learning_rate = lr
 class FlattenLayer:
     def __init__(self) -> None:
         self.input = None
@@ -159,7 +152,6 @@ class ReLuLayer:
         return np.maximum(0, input)
     
     def backward(self, error):
-        
         error[self.input < 0] = 0 
         return  error
 
@@ -174,7 +166,7 @@ class SoftmaxLayer:
         self.axis = axis
 
     def forward(self, input):
-        exps = np.exp(input - np.max(input, axis=self.axis, keepdims=True))  # Numerische Stabilität
+        exps = np.exp(input - np.max(input, axis=self.axis, keepdims=True)) 
         self.output = exps / np.sum(exps, axis=self.axis, keepdims=True)
         return self.output
 
@@ -190,16 +182,14 @@ def CNN_forward(image,layers):
         output = layer.forward(output)
  
     return (output)
+
+
 def CNN_backward(error_output,layers):
     error =error_output
-    #print("mean:",np.mean(error),"of start Error")
-    #print("var:",np.var(error),"of start Error")
-    #print("_____________________")
+
     for layer in reversed(layers):
         error = layer.backward(error) 
-        #print("mean:",np.mean(error),"of",type(layer))
-        #print("var:",np.var(error),"of",type(layer))
-        #print("sum:",np.sum(np.abs(error)),"of",type(layer))
+
 
 def normalizeData(data):    
     data[:, 1:] = data[:, 1:] / 255.0
@@ -212,80 +202,18 @@ def calculate_accuracy(predictions, labels):
     accuracy = (correct_predictions / len(labels)) * 100
     return accuracy
 
-def int_to_letter(n):
-    if 0 <= n <= 25:  
-        return chr(97 + n)
-    else:
-        raise ValueError("Die Zahl sollte zwischen 0 und 25 liegen.")
-def conv2d_valid(input_array, kernel, output_size=(3, 3)):
-     
 
-    x, y = input_array.shape
-    a, b = kernel.shape
-
-    output_x, output_y = output_size
-
-    output = np.zeros((output_x, output_y))
-
-    for i in range(output_x):
-        for j in range(output_y):
-            if i + a <= x and j + b <= y:
-                output[i, j] = np.sum(input_array[i:i+a, j:j+b] * kernel)
-            else:
-                # Bereich außerhalb der Eingabematrix mit Nullen auffüllen
-                temp_array = np.zeros((a, b))
-                temp_array[:min(x-i, a), :min(y-j, b)] = input_array[i:min(i+a, x), j:min(j+b, y)]
-                output[i, j] = np.sum(temp_array * kernel)
-
-    return output
-
-
-
-def conv2d_same(input_array, kernel):
-    
-    x, y = input_array.shape
-    a, b = kernel.shape
-
-    pad_x = a // 2
-    pad_y = b // 2
-
-    padded_input = np.pad(input_array, ((pad_x, pad_x), (pad_y, pad_y)), mode='constant')
-
-    output = np.zeros_like(input_array)
-
-    for i in range(x):
-        for j in range(y):
-            output[i, j] = np.sum(padded_input[i:i+a, j:j+b] * kernel)
-
-    return output
 
 def maxpooling2d(input_matrix, kernel_size):
-    # Get the dimensions of the input matrix
     rows, cols = input_matrix.shape
     
-    # Calculate the dimensions of the pooled matrix
     pooled_rows = rows // kernel_size
     pooled_cols = cols // kernel_size
-    
-    # Initialize the output matrix with zeros
     output_matrix = np.zeros((pooled_rows, pooled_cols))
     
-    # Perform max pooling and place the result in the center of the output matrix
     for i in range(pooled_rows):
         for j in range(pooled_cols):
             output_matrix[i , j ] = np.max(input_matrix[(kernel_size*i):(kernel_size*i)+kernel_size, (kernel_size*j):(kernel_size*j)+kernel_size])
     
     return output_matrix
 
-def correct_max_mask(max_masks, Kernel_size):
-    correct_max_mask = np.zeros_like(max_masks)
-    for i, max_mask in enumerate(max_masks):
-        for y in range(max_mask.shape[0]):
-            for x in range(max_mask.shape[1]):
-                s_x = x // Kernel_size
-                s_y = y // Kernel_size
-                patch = correct_max_mask[i, s_y*Kernel_size:s_y*Kernel_size+Kernel_size, s_x*Kernel_size:s_x*Kernel_size+Kernel_size]
-                patch_sum = np.sum(patch)
-                if patch_sum == 0:
-                    correct_max_mask[i, y, x] = max_mask[y, x]
-    return correct_max_mask
